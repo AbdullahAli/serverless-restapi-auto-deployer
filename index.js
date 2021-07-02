231ab734f779e9a676629209f4b9a2a6b2903796
Abdullah Ali <abdullah.ali@superdry.com>
2021-07-02 11:00:42 +0100

    WIP

--- index.js
+++ index.js
@@ -1,46 +1,39 @@
 'use strict';
-
 class ServerlessPlugin {
   constructor(serverless) {
     this.serverless = serverless;
     this.provider = this.serverless.getProvider('aws');
-    this.pluginName = 'APIGateway Auto Deployer';
-
-    this.hooks = {
-      'after:deploy:deploy': this.postDeploy.bind(this),
-    };
+    this.entity = 'RestAPI Automatic Deployment';
+    this.hooks = { 'after:deploy:deploy': this.postDeploy.bind(this) };
   }
-
   postDeploy() {
-    const stageName =
-      this.serverless.variables.options.stage ||
-      this.serverless.service.defaults.stage;
-
-    this.serverless.cli.log('STARTING_DEPLOY', this.pluginName);
-
-    console.log(this.serverless.service);
-
-    // const restApis = await this.provider.request('APIGateway', 'getRestApis');
-
-    // return this.provider
-    //   .request('APIGateway', 'getRestApis')
-    //   .then(({ items: restApis }) => restApis.filter(restApi => restApi.name === ));
-
-    // this.serverless.cli.log(
-    //   `DEPLOY_IN_PROGRESS - { restApiId: ${restApiId}, stageName: ${stageName} }`,
-    //   this.pluginName,
-    //   { color: 'green' }
-    // );
-
-    // await this.provider.request('APIGateway', 'createDeployment', {
-    //   restApiId,
-    //   stageName,
-    // });
-
-    // this.serverless.cli.log('DEPLOY_COMPLETE', this.pluginName, {
-    //   color: 'green',
-    // });
+    const stageName = this.serverless.service.provider.stage;
+    const restApiName = `${this.serverless.service.service}-${stageName}`;
+    return this.provider
+      .request('APIGateway', 'getRestApis', { limit: 500 })
+      .then(({ items: restApis }) =>
+        restApis.find((restApi) => restApi.name === restApiName)
+      )
+      .then(({ id: restApiId }) => {
+        this.serverless.cli.log(
+          `DEPLOY_IN_PROGRESS - ${JSON.stringify(
+            { restApiId, stageName, restApiName },
+            null,
+            4
+          )}`,
+          this.entity,
+          { color: 'green' }
+        );
+        return this.provider.request('APIGateway', 'createDeployment', {
+          restApiId,
+          stageName,
+        });
+      })
+      .then(() =>
+        this.serverless.cli.log('DEPLOY_COMPLETE', this.entity, {
+          color: 'green',
+        })
+      );
   }
 }
-
 module.exports = ServerlessPlugin;
